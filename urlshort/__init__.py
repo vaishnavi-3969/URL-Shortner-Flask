5ed6285161b32b0ce591d7ce59b55826c7ff28c6
Vaishnavi Arun Kale <vaishnavi.kale3011@gmail.com>
2023-03-31 23:53:09 +0530

    updated code

--- urlshort/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-def create(test_config=None):
-    app = Flask(__name__)
-    app.secret_key = 'vaishnavikale'
-    from . import urlshort
-    app.register_blueprint(urlshort.bp)
-    return app
